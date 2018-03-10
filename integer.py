8b0bb6eef812f54f0dfcb5c52a1afc987beece30
mugil23 <37238253+mugil23@users.noreply.github.com>
2018-03-10 15:03:57 +0530

    Create integer.py

---
+++ integer.py
@@ -0,0 +1,9 @@
+n = float(input("enter a number:"))
+if n>100000:
+    print("invalid input")
+elif n>0:
+    print("positive integer")
+elif n==0:
+    print("zero")
+else:
+    print("negative integer")
