421c06e17ff41d4e60e270863537045faf427ba9
mugil23 <37238253+mugil23@users.noreply.github.com>
2019-02-05 11:38:34 +0530

    Update alpha1.py

--- alpha1.py
+++ alpha1.py
@@ -1,8 +1,8 @@
-l = input("Input a letter of the alphabet: ")
+n = input("Input a letter of the alphabet: ")
 
-if l in ('a', 'e', 'i', 'o', 'u'):
-	print("%s is a vowel." % l)
-elif l == 'y':
+if n in ('a', 'e', 'i', 'o', 'u'):
+	print("%s is a vowel." % n)
+elif n == 'y':
 	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
 else:
-	print("%s is a consonant." % l)
+	print("%s is a consonant." % n)
