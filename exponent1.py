38431de90fac9a18ff7aec4a901c57ff289b271b
mugil23 <37238253+mugil23@users.noreply.github.com>
2019-02-05 14:15:54 +0530

    Update exponent1.py

--- exponent1.py
+++ exponent1.py
@@ -3,6 +3,6 @@ def power(base,ex):
         return(base)
     if(ex!=1):
         return(base*power(base,ex-1))
-base=int(input("Enter base: "))
-exp=int(input("Enter exponential value: "))
-print("Result:",power(base,ex))
+base=int(input("input: "))
+ex=int(input(" "))
+print(power(base,ex))
