4eec4864ebff621cd6348e987f9ff1bdf89d6aba
mugil23 <37238253+mugil23@users.noreply.github.com>
2019-02-05 11:48:10 +0530

    Update alpha1.py

--- alpha1.py
+++ alpha1.py
@@ -1,8 +1,5 @@
-n = input("Input a letter of the alphabet: ")
-
-if n in ('a', 'e', 'i', 'o', 'u'):
-	print("%s is a vowel." % n)
-elif n == 'y':
-	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+l = input()
+if l.isalpha():
+	print("alphabatic order")
 else:
-	print("%s is a consonant." % n)
+  print("not alphabatic order")
