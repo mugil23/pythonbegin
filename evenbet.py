0066e179ffcb3046591f10b4dcfe3579e428fc58
mugil23 <37238253+mugil23@users.noreply.github.com>
2019-02-05 15:28:33 +0530

    Create evenbet.py

---
+++ evenbet.py
@@ -0,0 +1,10 @@
+try:
+    a=int(input(""))
+    b=int(input(""))
+    for i in range(a,b):
+        if(i%2==0):
+            print (i)
+        elif(a==(b-a)):
+            print("no even number in this limit")
+except:
+  print("Invalid entry")
