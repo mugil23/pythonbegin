030bac4cfa6c68ac36a941d4027fdb5f6968ffb9
mugil23 <37238253+mugil23@users.noreply.github.com>
2019-02-05 12:13:11 +0530

    Update leapyr.py

--- leapyr.py
+++ leapyr.py
@@ -1,10 +1,10 @@
 yr = int(input("enter the year: "))
 
 if yr % 4 == 0 and yr % 100 != 0:
-    print(yr, "is a Leap Year")
+    print( "yes")
 elif yr % 100 == 0:
-    print(yr, "is not a Leap Year")
+    print( "no")
 elif yr % 400 ==0:
-    print(yr, "is a Leap Year")
+    print("yes")
 else:
-    print(yr, "is not a Leap Year")
+    print( "no")
