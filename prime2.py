0f46797bfc51c66de378bee450380e7d19b8e93d
mugil23 <37238253+mugil23@users.noreply.github.com>
2019-02-05 14:45:14 +0530

    Update prime2.py

--- prime2.py
+++ prime2.py
@@ -1,12 +1,11 @@
-number = int(input(" "))
+num = int(input(" "))
 
-# prime number is always greater than 1
-if number > 1:
-    for i in range(2, number):
-        if (number % i) == 0:
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
             print('no')
             break
     else:
         print('yes')
 else:
-  print(number, "is not a prime number")
+  print(num, "is not a prime number")
