5a8dfe39e9aeb09a87e8a7843daf7a20f2a98576
mugil23 <37238253+mugil23@users.noreply.github.com>
2019-02-06 15:25:40 +0530

    Create mul1.py

---
+++ mul1.py
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        longint sum=0,i,j;
+        clrscr();
+        printf("");
+        scanf("%ld",&j);
+        for(i=1;i<=j;i++)
+        {
+            printf("\n 5 * %ld = %ld",i,5*i);
+        }
+        
+        getch();
+}
