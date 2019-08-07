bb3114c705c6cee34b55e1e241942b675ee90cdf
mugil23 <37238253+mugil23@users.noreply.github.com>
2019-08-07 22:39:41 +0530

    Update remvow.py

--- remvow.py
+++ remvow.py
@@ -1,6 +1,5 @@
+n = int(input())
 m = input("")
-print(len(m))
-print(m)
 s = ""
 for i in m:
   if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
