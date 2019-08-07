d54e39b0403ccd71cfb9aa8f3088f4ffe91370a0
mugil23 <37238253+mugil23@users.noreply.github.com>
2019-08-07 22:32:03 +0530

    Create remvow.py

---
+++ remvow.py
@@ -0,0 +1,9 @@
+m = input("")
+s = ""
+for i in m:
+  if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+    pass
+  else:
+    s+=i
+m = s
+print(m)
